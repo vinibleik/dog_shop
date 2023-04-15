246323b40943d114bddbd1e69a3fb62fd3fc773c
vinicius <vinicius_baggio@hotmail.com>
2023-04-15 19:35:52 -0300

    Product class done

---
+++ Product.js
@@ -0,0 +1,23 @@
+class Product {
+    static #products = [];
+
+    constructor(name, price, img = "") {
+        this.id = this.constructor.#products.length;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+
+        this.constructor.#products.push(this);
+    }
+
+    static searchId(id) {
+        if (typeof id !== "number") throw new TypeError("id must be a number");
+        id = parseInt(id);
+        if (id < 0 || id >= this.#products.length) return null;
+        return this.#products[id];
+    }
+}
+
+module.exports = {
+    Product,
+};
